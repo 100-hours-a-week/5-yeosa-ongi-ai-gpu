0ac989bd6bb5cc585595134978d449d5b36eb224
Kim Dahyun <ganghwapeople@gmail.com>
2025-05-20 10:48:04 +0900

    feat: 이미지 로더 최신화(비동기 sdk, 비동기 구조 개선)

--- app/utils/image_loader.py
+++ app/utils/image_loader.py
@@ -1,18 +1,22 @@
-import os, requests
+import os
+import asyncio, time, requests
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
 
+import aiofiles
 import boto3
+import aioboto3
+import cv2
+import numpy as np
 from dotenv import load_dotenv
-from google.cloud import storage
-from PIL import Image
+from botocore.config import Config
+from gcloud.aio.storage import Storage
 from starlette.concurrency import run_in_threadpool
 
 from app.config.settings import ImageMode, APP_ENV, AppEnv
 
 load_dotenv()
-
 
 # local
 LOCAL_IMG_PATH_raw = os.getenv("LOCAL_IMG_PATH")
@@ -57,6 +61,19 @@ AWS_ACCESS_KEY_ID: str = AWS_ACCESS_KEY_ID_raw
 AWS_SECRET_ACCESS_KEY: str = AWS_SECRET_ACCESS_KEY_raw
 AWS_REGION: str = AWS_REGION_raw
 
+# 공통 디코더
+def decode_image_cv2(image_bytes: bytes, label: str) -> np.ndarray:
+    #start = time.time()
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #end = time.time()
+    #start2 = time.time()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #end2 = time.time()
+    #print(f"디코딩 시간({label}): {end - start:.6f}초")
+    #print(f"색상공간 변환 시간({label}): {end2 - start2:.6f}초")
+    return img
+
 class BaseImageLoader(ABC):
     """
     이미지 로더의 추상 베이스 클래스.
@@ -65,7 +82,7 @@ class BaseImageLoader(ABC):
     """
 
     @abstractmethod
-    async def load_images(self, filenames: list[str]) -> list[Image.Image]:
+    async def load_images(self, filenames: list[str]) -> list[bytes]:
         """
         주어진 이미지 파일 이름 리스트에 대해 이미지를 로드합니다.
 
@@ -73,7 +90,7 @@ class BaseImageLoader(ABC):
             filenames (list[str]): 로드할 이미지 파일 이름 목록
 
         Returns:
-            list[Image.Image]: 로드된 PIL 이미지 객체 리스트
+            list[bytes]: 로드된 이미지 바이트 리스트
 
         """
         pass
@@ -90,7 +107,19 @@ class LocalImageLoader(BaseImageLoader):
         """
         self.image_dir = image_dir
 
-    async def load_images(self, filenames: list[str]) -> list[Image.Image]:
+    async def _load_single_image(self, filename: str) -> np.ndarray:
+        file_path = os.path.join(self.image_dir, filename)
+
+        # 1. 파일 비동기 I/O로 읽기
+        async with aiofiles.open(file_path, mode="rb") as f:
+            image_bytes = await f.read()
+
+        # 2. 디코딩은 스레드에서 실행
+        loop = asyncio.get_running_loop()
+        decoded_img = await loop.run_in_executor(None, decode_image_cv2, image_bytes, "local")
+        return decoded_img
+
+    async def load_images(self, filenames: list[str]) -> list[np.ndarray]:
         """
         비동기적으로 로컬 이미지들을 로드합니다.
 
@@ -98,17 +127,11 @@ class LocalImageLoader(BaseImageLoader):
             filenames (list[str]): 이미지 파일 이름 리스트
 
         Returns:
-            list[Image.Image]: 로드된 이미지 리스트
+            list[np.ndarray]: 로드된 이미지 리스트
 
         """
-        return await run_in_threadpool(
-            lambda: list(
-                Image.open(os.path.join(self.image_dir, filename)).convert(
-                    "RGB"
-                )
-                for filename in filenames
-            )
-        )
+        tasks = [self._load_single_image(name) for name in filenames]
+        return await asyncio.gather(*tasks)
 
 
 class GCSImageLoader(BaseImageLoader):
@@ -123,11 +146,10 @@ class GCSImageLoader(BaseImageLoader):
             key_path (str): 서비스 계정 키 경로 (.json)
 
         """
-        self.client = storage.Client.from_service_account_json(key_path)
-        self.bucket = self.client.bucket(bucket_name)
-        self.executor = ThreadPoolExecutor(max_workers=10)
+        self.client = Storage(service_file=key_path)
+        self.bucket_name = bucket_name
 
-    def _download(self, file_name: str) -> Image.Image:
+    async def _download(self, file_name: str) -> bytes:
         """
         GCS에서 단일 이미지를 다운로드하고 RGB로 변환합니다.
 
@@ -135,16 +157,29 @@ class GCSImageLoader(BaseImageLoader):
             file_name (str): GCS 내 파일 이름
 
         Returns:
-            Image.Image: 로드된 PIL 이미지
+            bytes: 로드된 이미지 바이트
 
         """
-        
-        
-        blob = self.bucket.blob(file_name)
-        image_bytes = blob.download_as_bytes()
-        return Image.open(BytesIO(image_bytes)).convert("RGB")
+        #start = time.time()
+        image_bytes = await self.client.download(
+            bucket=self.bucket_name, object_name=file_name
+        )
+        # image_bytes = await blob.download_as_bytes(client=self.client)
+        #end = time.time()
+        #print(f" GCS 다운로드 시간 : {end - start}")
+        return image_bytes
 
-    async def load_images(self, filenames: list[str]) -> list[Image.Image]:
+    async def _process_single_file(
+        self, filename: str, executor=None
+    ) -> np.ndarray:
+        loop = asyncio.get_running_loop()
+        image_bytes = await self._download(filename)
+        decoded_img = await loop.run_in_executor(
+            executor, decode_image_cv2, image_bytes, "gcs"
+        )
+        return decoded_img
+
+    async def load_images(self, filenames: list[str]) -> list[np.ndarray]:
         """
         비동기적으로 GCS에서 이미지를 병렬로 다운로드합니다.
 
@@ -152,12 +187,16 @@ class GCSImageLoader(BaseImageLoader):
             filenames (list[str]): GCS 상의 이미지 파일 이름 리스트
 
         Returns:
-            list[Image.Image]: 로드된 이미지 리스트
+            list[np.ndarray]: 로드된 이미지 리스트
 
         """
-        return await run_in_threadpool(
-            lambda: list(self.executor.map(self._download, filenames))
-        )
+        #start = time.time()
+        tasks = [self._process_single_file(f, None) for f in filenames]
+
+        result = await asyncio.gather(*tasks)
+        #end = time.time()
+        #print(f"전체 시간(gcs): {end - start:.6f}초")
+        return result
 
 class S3ImageLoader(BaseImageLoader):
     """Amazon S3에서 이미지를 로드하는 클래스입니다."""
@@ -178,15 +217,29 @@ class S3ImageLoader(BaseImageLoader):
 
         """
         self.bucket_name = bucket_name
-        self.s3 = boto3.client(
-            "s3",
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            region_name=region_name,
-        )
-        self.executor = ThreadPoolExecutor(max_workers=10)
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.region_name = region_name
+        self.session = aioboto3.Session()
+        self.client = None
 
-    def _download(self, file_ref: str) -> Image.Image:
+    async def init_client(self):
+        """S3 클라이언트 초기화 (lifespan 시작 시 호출됨)"""
+        self.client = await self.session.client(
+            "s3",
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=self.region_name,
+            config=Config(retries={"max_attempts": 3}),
+        ).__aenter__()
+
+    async def close_client(self):
+        """S3 클라이언트 종료 (lifespan 끝날 때 호출됨)"""
+        if self.client:
+            await self.client.__aexit__(None, None, None)
+            self.client = None
+
+    async def _download(self, file_ref: str) -> bytes:
         """
         S3에서 단일 이미지를 다운로드하고 RGB로 변환합니다.
 
@@ -194,23 +247,40 @@ class S3ImageLoader(BaseImageLoader):
             file_ref (str): S3 내 파일 이름 (key)
 
         Returns:
-            Image.Image: 로드된 PIL 이미지 객체
+            bytes: 로드된 이미지 바이트
 
         """
+        #start = time.time()
         if APP_ENV == AppEnv.PROD:
             try:
                 response = requests.get(file_ref, timeout=5)
                 response.raise_for_status()
-                return Image.open(BytesIO(response.content)).convert("RGB")
-            except Exception as e:
-                raise RuntimeError(f"[S3ImageLoader:prod] 이미지 다운로드 실패 URL: {file_ref}") from e
+                image_bytes = response.content
+                #end = time.time()
+                #print(f" S3 (URL 직접) 다운로드 시간 : {end - start}")
+                return image_bytes
+            except requests.RequestException as e:
+                print(f" S3 URL 요청 실패: {file_ref}, 오류: {e}")
+                raise
         else:
-            response = self.s3.get_object(Bucket=self.bucket_name, Key=file_ref)
-            image_bytes = response["Body"].read()
-            return Image.open(BytesIO(image_bytes)).convert("RGB")
+            # 이미지명(key) 기반 로딩
+            response = await self.client.get_object(
+                Bucket=self.bucket_name, Key=file_ref
+            )
+            image_bytes = await response["Body"].read()
+            #end = time.time()
+            #print(f" S3 다운로드 시간 : {end - start}")
+            return image_bytes
     
+    async def _process_single_file(self, filename: str) -> np.ndarray:
+        loop = asyncio.get_running_loop()
+        image_bytes = await self._download(filename)
+        decoded = await loop.run_in_executor(
+            None, decode_image_cv2, image_bytes, "s3"
+        )
+        return decoded
 
-    async def load_images(self, filenames: list[str]) -> list[Image.Image]:
+    async def load_images(self, filenames: list[str]) -> list[np.ndarray]:
         """
         비동기적으로 S3에서 이미지를 병렬로 다운로드합니다.
 
@@ -218,12 +288,15 @@ class S3ImageLoader(BaseImageLoader):
             filenames (list[str]): S3 내 이미지 파일 이름 리스트
 
         Returns:
-            list[Image.Image]: 로드된 PIL 이미지 리스트
+            list[np.ndarray]: 로드된 이미지 바이트 리스트
 
         """
-        return await run_in_threadpool(
-            lambda: list(self.executor.map(self._download, filenames))
-        )
+        #start = time.time()
+        tasks = [self._process_single_file(f) for f in filenames]
+        result = await asyncio.gather(*tasks)
+        #end = time.time()
+        #print(f"전체 시간(s3): {end - start:.6f}초")
+        return result
 
 
 def get_image_loader(mode: ImageMode) -> BaseImageLoader:
