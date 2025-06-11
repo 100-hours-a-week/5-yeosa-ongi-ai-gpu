3b3559973eb52d82e6d0be632012b48f2f9e92c2
이상원 <simon.lee@isang-won-ui-MacBookPro.local>
2025-06-11 14:38:36 +0900

    test: cpu no batch 64

--- app/service/embedding.py
+++ app/service/embedding.py
@@ -11,26 +11,26 @@ def embed_images(
     # 이미지 전처리를 배치 단위로 수행
     preprocessed_batches = []
     # print(f"[INFO] 전처리 시작")
-    # t1 = time.time()
+    t1 = time.time()
     # ThreadPoolExecutor 생성
-    # with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor() as executor:
         # 전처리 함수를 lambda로 정의
-        # preprocess_func = lambda img: preprocess(img)
+        preprocess_func = lambda img: preprocess(img)
         
-    for i in range(0, len(images), batch_size):
-        batch_images = images[i:i + batch_size]
-        # 병렬로 전처리 수행
-        # preprocessed_batch = list(executor.map(preprocess_func, batch_images))
-        # preprocessed_batch = torch.stack(preprocessed_batch)
-        preprocessed_batch = preprocess(batch_images)
-        preprocessed_batches.append(preprocessed_batch)
-    # t2 = time.time()
-    # print(f"[INFO] 전처리 완료: {format_elapsed(t2 - t1)}")
+        for i in range(0, len(images), batch_size):
+            batch_images = images[i:i + batch_size]
+            # 병렬로 전처리 수행
+            preprocessed_batch = list(executor.map(preprocess_func, batch_images))
+            preprocessed_batch = torch.stack(preprocessed_batch)
+            # preprocessed_batch = preprocess(batch_images)
+            preprocessed_batches.append(preprocessed_batch)
+    t2 = time.time()
+    print(f"[INFO] 전처리 완료: {format_elapsed(t2 - t1)}")
     # 결과를 저장할 딕셔너리
     results = {}
     
     # print(f"[INFO] 임베딩 시작")
-    # t1 = time.time()
+    t1 = time.time()
     # 배치 단위로 임베딩 수행
     for i, batch in enumerate(preprocessed_batches):
         batch_filenames = filenames[i * batch_size:(i + 1) * batch_size]
@@ -47,7 +47,7 @@ def embed_images(
         for filename, feature in zip(batch_filenames, batch_features):
             results[filename] = feature.cpu()
 
-    # t2 = time.time()
-    # print(f"[INFO] 임베딩 완료: {format_elapsed(t2 - t1)}")
+    t2 = time.time()
+    print(f"[INFO] 임베딩 완료: {format_elapsed(t2 - t1)}")
 
     return results
